b0b50321c2418e438680ad79ce4677147e551870
Gonzalo corvalan <salvatore.hakase@gmail.com>
2023-05-17 16:16:20 -0300

    TsumugiHotel component is finished!

--- components/Home/WhereIHaveWorked/Descriptions/TsumugiHotel.tsx
+++ components/Home/WhereIHaveWorked/Descriptions/TsumugiHotel.tsx
@@ -2,19 +2,19 @@ import React from "react";
 import ArrowIcon from "../../../Icons/ArrowIcon";
 import { getTasksTextWithHighlightedKeyword } from "./taskAndType";
 
-export default function TrouveTavoie() {
+export default function TsumugiHotel() {
   const tasks = [
     {
-      text: "Spearhead & implemented a new design user workflow system for the Frond-End Architecture of a NFT Marketplace.",
-      keywords: ["NFT Marketplace"],
+      text: "Highly experienced hotel receptionist with a proven track record of 9 months working in Japan.",
+      keywords: ["Highly experienced", 'Japan'],
     },
     {
-      text: "Work with a variety of different languages, platforms, frameworks, and content management systems such as JavaScript, TypeScript, Next.js/React, AWS and Vercel.",
-      keywords: ["Next.js/React", "AWS", "Vercel"],
+      text: "Skillfully managed guest check-ins and check-outs while delivering exceptional customer service.",
+      keywords: ["exceptional customer service"],
     },
     {
-      text: "Interfaced with developers on a daily basis, providing technological expertise.",
-      keywords: [],
+      text: "Effective communicator and proficient in handling reservations, providing accurate information, and ensuring a an enjoyable guest experience.",
+      keywords: ['Effective communicator'],
     },
   ];
 
@@ -24,16 +24,16 @@ export default function TrouveTavoie() {
         <div className="flex flex-col spacey-y-2">
           {/* Title */}
           <span className="text-gray-100 sm:text-lg text-sm font-Arimo tracking-wide">
-            Software Engineer <span className="text-AAsecondary">@ Blockchain</span>
+            Hotel Receptionist <span className="text-AAsecondary">@ Tsumugi Hotel</span>
           </span>
           {/* Date */}
-          <span className="font-mono text-xs text-gray-500">Mars - August 2022</span>
+          <span className="font-mono text-xs text-gray-500">Kyoto, Japan {'<'}- -{'>'} Jun 2022 - Mar 2023</span>
         </div>
         <div className="flex flex-col space-y-4 sm:text-sm text-xs">
           {/* Tasks Description 1 */}
           {tasks.map((item, index) => {
             return (
-              <div key={index} className="flex flex-row space-x-1">
+              <div key={item.text + index} className="flex flex-row space-x-1">
                 <ArrowIcon className={" h-5 w-4 text-AAsecondary flex-none"} />
                 <span
                   className="text-gray-500 sm:text-sm text-xs"
