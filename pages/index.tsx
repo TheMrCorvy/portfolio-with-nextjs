ddd4b6a4b3157ae7d3c49e58126325427959c931
Salvatore Hakase <themrcorvy@MacBook-Pro-de-Salvatore.local>
2023-05-17 13:26:06 -0300

    working on the Hero page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
+import { useContext, useEffect, useState, useRef } from "react";
+import Head from "next/head";
+
 import Header from "../components/Header/Header";
 import Startup from "../components/Header/StartupLogo/Startup";
 import MyName from "../components/Home/MyName/MyName";
-import { useContext, useEffect, useState, useRef } from "react";
 import SocialMediaArround from "../components/Home/SocialMediaArround/SocialMediaArround";
 import AboutMe from "../components/Home/AboutMe/AboutMe";
 import ThisCantBeReached from "../components/Home/ThisSiteCantBeReached/ThisCantBeReached";
@@ -10,9 +12,10 @@ import SomethingIveBuilt from "../components/Home/SomethingIveBuilt/SomethingIve
 import GetInTouch from "../components/Home/GetInTouch/GetInTouch";
 import Footer from "../components/Footer/Footer";
 import AppContext from "../components/AppContextFolder/AppContext";
+
 import Aos from "aos";
 import "aos/dist/aos.css";
-import Head from "next/head";
+
 export default function Home() {
   const [ShowElement, setShowElement] = useState(false);
   const [ShowThisCantBeReached, setShowThisCantBeReached] = useState(true);
@@ -53,16 +56,17 @@ export default function Home() {
     Aos.init({ duration: 2000, once: true });
   }, []);
 
- 
+
 
   console.log("Portfolio Rendered...");
+
   const meta = {
-    title: "Abdellatif Anaflous - Software Engineer",
-    description: `I've been working on Software development for 5 years straight. Get in touch with me to know more.`,
-    image: "/titofCercle.png",
+    title: "Corvalan Gonzalo",
+    description: `I've been working on Software development for 2 years straight. Get in touch with me to know more.`,
+    image: "/titofCercle.png", // cambiar
     type: "website",
   };
- 
+
   return (
     <>
       <Head>
